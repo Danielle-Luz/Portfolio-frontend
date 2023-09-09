537554e33a0b368fcc2a936c64543d455a92b627
Danielle Luz <daniln2209@gmail.com>
2023-09-08 21:09:37 -0300

    feat: create project component

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,17 @@
+import { tProject, tStack } from "../../interfaces";
+import { StyledButton, StyledProject } from "../../styles";
+
+export const Project = (project: tProject, projectStack: tStack) => {
+  const isFrontend = projectStack === "Front-end";
+
+  return (
+    <StyledProject>
+      {isFrontend && <img src={project.coverImage} />}
+      <section>
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+      </section>
+      <StyledButton variant="outline">Repositório</StyledButton>
+    </StyledProject>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { Header } from "./header";
 export { ProfileCard } from "./ProfileCard";
+export { Project } from "./Project";
