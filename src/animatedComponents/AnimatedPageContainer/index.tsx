233fa315e03fd394ecb3299a462e720acee59499
Danielle Luz <daniln2209@gmail.com>
2023-09-10 13:58:18 -0300

    feat: add animated page options

--- src/animatedComponents/AnimatedPageContainer/index.tsx
+++ src/animatedComponents/AnimatedPageContainer/index.tsx
@@ -1,6 +1,16 @@
 import { tParentComponentProps } from "../../interfaces";
 import { StyledAnimatedPageContainer } from "../../styles";
+import { animatedPageContainerOptions } from "../animationOptions";
 
 export const AnimatedPageContainer = ({ children }: tParentComponentProps) => {
-  return <StyledAnimatedPageContainer>{children}</StyledAnimatedPageContainer>;
+  return (
+    <StyledAnimatedPageContainer
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={animatedPageContainerOptions}
+    >
+      {children}
+    </StyledAnimatedPageContainer>
+  );
 };
